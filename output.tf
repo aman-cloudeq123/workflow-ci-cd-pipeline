3f62b62e2812094a00c87553a683ca844cadadd2
aman-cloudeq123 <101627403+aman-cloudeq123@users.noreply.github.com>
2022-05-10 22:55:29 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "security_group_public" {
   value = aws_security_group.public-grp.id
 }
  
-# output "security_group_private" {
-#   value = aws_security_group.private-grp1.id
-# }
+output "security_group_private" {
+  value = aws_security_group.private-grp1.id
+}
